c589ca182f4a78a4cc03a7d1a4f739d6afc4e459
jennifernatis <jennifer.natis@gmail.com>
2020-11-25 20:42:32 -0500

    Archivo repaso 3 creación de estructura

---
+++ src/estudio/reforzando/TablaDeVerdad3.java
@@ -0,0 +1,14 @@
+package estudio.reforzando;
+
+public class TablaDeVerdad3 {
+
+public static void main (String [] args)
+{
+//  System.out.println(true + " AND " + true +" = "+ (true&&true) ); 
+  System.out.println(" true AND true = "+ (true&&true) );
+  System.out.println(" true AND false = "+ (true&&false) ); 
+	
+System.out.println("cualquier otra tabla de verdad");
+System.out.println("quiero hacer algo. practicar mas.. . ");
+}
+}
